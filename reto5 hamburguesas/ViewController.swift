b67d193326ca31d07b9665d14149b75510215945
macsierra <macsierra@macsierras-iMac.local>
2017-04-23 18:50:06 -0500

    Initial Commit

---
+++ reto5 hamburguesas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reto5 hamburguesas
+//
+//  Created by macsierra on 4/23/17.
+//  Copyright © 2017 Daniel's inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
